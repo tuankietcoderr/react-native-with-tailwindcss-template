e89d93c8ceac335fdecd655106a51278cb40f7c6
tuankietcoder <charlieismylife2003@gmail.com>
2022-09-07 23:47:09 +0700

    Add some information

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StatusBar as ReactNativeStatusBar } from "react-native";
 import { View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { Home } from "./screens";
@@ -11,11 +12,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <View style={{ marginTop: StatusBar.currentHeight }} />
+        {/* Apply this line when Expo Status Bar doesn't work */}
+        {/* <View style={{ marginTop: ReactNativeStatusBar.currentHeight }} /> */}
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           {/* Stack.Screen */}
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
+        <StatusBar style="auto" hidden />
       </TailwindProvider>
     </NavigationContainer>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center gap-y-6">
+      <View className="flex-row gap-x-4">
+        <Image
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/220px-React-icon.svg.png",
+          }}
+          className="w-20 h-20"
+          resizeMode="contain"
+        />
+        <Image
+          source={{
+            uri: "https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/299463877_560030875914611_8025210373789510385_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=W3bFryOoYeQAX8mz5qx&tn=H2jKtPqRbQ0ESHfr&_nc_ht=scontent.fsgn5-9.fna&oh=00_AT_HvhxGWwasm9xEfWRTeaF8oRAfGIi0Y_joQRzCEzV9Rg&oe=631D2DF7",
+          }}
+          className="w-20 h-20 rounded-full"
+          resizeMode="contain"
+        />
+        <Image
+          source={{
+            uri: "https://www.saashub.com/images/app/service_logos/155/7902ojac7nj3/large.png?1635277061",
+          }}
+          className="w-20 h-20 rounded-full"
+          resizeMode="contain"
+        />
+      </View>
+      <Text className="font-extrabold text-md text-sky-700">
+        React Native + TailwindCSS + Heroicons Template
+      </Text>
+      <Text>by tuankietcoder</Text>
     </View>
   );
 };
